148f81a1684a199b24d4cdc7c403b5f76056f22c
Liliana <Liliana@DESKTOP-6KF85CQ>
2024-04-09 12:02:28 -0500

    Mi primer commit ejercicio calculadora

---
+++ src/calculadoranotas2/CalculadoraNotas2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadoranotas2;
+
+/**
+ *
+ * @author Liliana
+ */
+public class CalculadoraNotas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaCalculadora obj = new VentanaCalculadora();
+        obj.setVisible(true);
+                
+    }
+    
+}
